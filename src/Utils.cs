8a1c281186a6cf54baec0c351c82d62641eeb950
thalber <73020111+thalber@users.noreply.github.com>
2022-10-14 06:49:03 +0400

    prune utils

--- src/Utils.cs
+++ src/Utils.cs
@@ -153,7 +153,21 @@ namespace Atmo
         }
         #endregion
         #region misc bs
-
+        public static bool TryParseEnum<T> (string str, out T result)
+            where T : Enum
+        {
+            var values = Enum.GetValues(typeof(T));
+            foreach (T val in values)
+            {
+                if (str == val.ToString())
+                {
+                    result = val;
+                    return true;
+                }
+            }
+            result = default;
+            return false;
+        }
         public static void LogFrameTime(
             List<TimeSpan> realup_times,
             TimeSpan frame,
@@ -216,49 +230,6 @@ namespace Atmo
                 return (bf is null) ? null : enc.GetString(bf);
             }
             catch (Exception ee) { inst.Plog.LogError($"Error getting ER: {ee}"); return null; }
-        }
-        #endregion
-        #region weakdicts
-        //moved from M4rbleL1ne's ConditionalEffects
-        public static void RemoveWeak<T>(Dictionary<WeakReference, T> dict, RoomSettings.RoomEffect key)
-        {
-            WeakReference weakKey = null;
-            foreach (var pair in dict)
-                if (pair.Key.IsAlive && pair.Key.Target == key)
-                {
-                    weakKey = pair.Key;
-                    break;
-                }
-            if (weakKey != null)
-                dict.Remove(weakKey);
-        }
-        public static bool TryGetWeak<T>(Dictionary<WeakReference, T> dict, RoomSettings.RoomEffect key, out T value)
-        {
-            foreach (var pair in dict)
-                if (pair.Key.IsAlive && pair.Key.Target == key)
-                {
-                    value = pair.Value;
-                    return true;
-                }
-            value = default;
-            return false;
-        }
-        public static T GetWeak<T>(Dictionary<WeakReference, T> dict, RoomSettings.RoomEffect key)
-        {
-            foreach (var pair in dict)
-                if (pair.Key.IsAlive && pair.Key.Target == key)
-                    return pair.Value;
-            throw new KeyNotFoundException("Could not get from weak list");
-        }
-        public static void SetWeak<T>(Dictionary<WeakReference, T> dict, RoomSettings.RoomEffect key, T value)
-        {
-            foreach (var pair in dict)
-                if (pair.Key.IsAlive && pair.Key.Target == key)
-                {
-                    dict[pair.Key] = value;
-                    return;
-                }
-            dict[new WeakReference(key)] = value;
         }
         #endregion
     }
