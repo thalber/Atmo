f7c3cf1f16fc0a7f423574ce6a8d2be3b36f0b0c
thalber <73020111+thalber@users.noreply.github.com>
2022-10-16 05:56:59 +0400

    start bringing defaultcbs to separate methods

--- src/HappenBuilding.DefaultCB.cs
+++ src/HappenBuilding.DefaultCB.cs
@@ -30,138 +30,156 @@ internal static partial class HappenBuilding
                 case "playergrav":
                 case "playergravity":
                     {
-                        float.TryParse(action.Value.AtOr(0, "0.5"), out var frac);
-                        ha.On_RealUpdate += (room) =>
-                        {
-                            for (int i = 0; i < room.updateList.Count; i++)
-                            {
-                                UAD uad = room.updateList[i];
-                                if (uad is Player p) p.gravity = frac;
-                            }
-                        };
+                        Make_Playergrav(ha, args);
                     }
                     break;
                 //plays a sound. args: sound name, cooldown/cd, volume/vol, pitch, pan
                 case "sound":
                 case "playsound":
                     {
-                        if (!TryParseEnum(args[0],
-                            out SoundID soundid)) break;
-                        int cooldown = -1;
-                        float
-                            vol = 1f,
-                            pitch = 1f,
-                            pan = 1f;
-                        for (int i = 1; i < args.Length; i++)
-                        {
-                            var spl = TXT.Regex.Split(args[i], "=");
-                            if (spl.Length != 2) continue;
-                            switch (spl[0].ToLower())
-                            {
-                                case "cd":
-                                case "cooldown":
-                                    int.TryParse(spl[1], out cooldown);
-                                    break;
-                                case "vol":
-                                case "volume":
-                                    float.TryParse(spl[1], out vol);
-                                    break;
-                                case "pan":
-                                    float.TryParse(spl[1], out pan);
-                                    break;
-                                case "pitch":
-                                    float.TryParse(spl[1], out pitch);
-                                    break;
-                            }
-                        }
-                        int counter = 0;
-                        ha.On_RealUpdate += (room) =>
-                        {
-                            if (counter != 0) return;
-                            for (int i = 0; i < room.updateList.Count; i++)
-                            {
-                                if (room.updateList[i] is Player p)
-                                {
-                                    var em = room.PlaySound(soundid, p.firstChunk, false, vol, pitch);
-                                    counter = cooldown;
-                                    return;
-                                }
-                            }
-                        };
-                        ha.On_CoreUpdate += (rwg) =>
-                        {
-                            if (counter > 0) counter--;
-                        };
-
+                        Make_sound(ha, args);
                     }
                     break;
                 case "rumble":
                 case "screenshake":
                     {
-                        int cooldown = 200,
-                            duration = 80;
-                        float intensity = 1f,
-                            shake = 0.5f;
-
-                        for (int i = 0; i < args.Length; i++)
-                        {
-                            var spl = TXT.Regex.Split(args[i], "=");
-                            if (spl.Length != 2) continue;
-                            switch (spl[0].ToLower())
-                            {
-                                case "cooldown":
-                                case "cd":
-                                    int.TryParse(spl[1], out cooldown);
-                                    break;
-                                case "duration":
-                                    int.TryParse(spl[1], out duration);
-                                    break;
-                                case "intensity":
-                                    float.TryParse(spl[1], out intensity);
-                                    break;
-                                case "shake":
-                                    float.TryParse(spl[1], out shake);
-                                    break;
-                            }
-                        }
-
-                        int counter = 0;
-                        int active = duration;
-
-                        ha.On_CoreUpdate += (rwg) =>
-                        {
-                            if (active > 0) active--;
-                            if (counter > 0) counter--;
-                            if (counter == 0)
-                            {
-                                active = duration;
-                                counter = cooldown;
-                            }
-                            
-                        };
-                        ha.On_RealUpdate += (room) =>
-                        {
-                            if (active != 0) room.ScreenMovement(null, URand.insideUnitCircle * intensity, shake);
-                        };
+                        Make_Rumble(ha, args);
                     }
                     break;
-                //case "palette":
-                //case "palettefade":
-                //    {
-                //        int.TryParse(args.AtOr(0, "15"), out var palNum);
-                //        for (int i = 1; i < args.Length; i++)
-                //        {
+                    //case "palette":
+                    //case "palettefade":
+                    //    {
+                    //        int.TryParse(args.AtOr(0, "15"), out var palNum);
+                    //        for (int i = 1; i < args.Length; i++)
+                    //        {
 
-                //        }
-                //        ha.On_RealUpdate += (room) =>
-                //        {
+                    //        }
+                    //        ha.On_RealUpdate += (room) =>
+                    //        {
 
-                //        };
-                //    }
-                //    break;
-                //case "":
-                //    break;
+                    //        };
+                    //    }
+                    //    break;
+                    //case "":
+                    //    break;
             }
         }
+    }
+
+    private static void Make_Playergrav(Happen ha, string[] args)
+    {
+        float.TryParse(args.AtOr(0, "0.5"), out var frac);
+        ha.On_RealUpdate += (room) =>
+        {
+            for (int i = 0; i < room.updateList.Count; i++)
+            {
+                UAD uad = room.updateList[i];
+                if (uad is Player p) p.gravity = frac;
+            }
+        };
+    }
+
+    private static void Make_sound(Happen ha, string[] args)
+    {
+        if (!TryParseEnum(args[0], out SoundID soundid))
+        {
+            return;
+        }
+
+        int cooldown = -1;
+        float
+            vol = 1f,
+            pitch = 1f,
+            pan = 1f;
+        for (int i = 1; i < args.Length; i++)
+        {
+            var spl = TXT.Regex.Split(args[i], "=");
+            if (spl.Length != 2) continue;
+            switch (spl[0].ToLower())
+            {
+                case "cd":
+                case "cooldown":
+                    int.TryParse(spl[1], out cooldown);
+                    break;
+                case "vol":
+                case "volume":
+                    float.TryParse(spl[1], out vol);
+                    break;
+                case "pan":
+                    float.TryParse(spl[1], out pan);
+                    break;
+                case "pitch":
+                    float.TryParse(spl[1], out pitch);
+                    break;
+            }
+        }
+        int counter = 0;
+        ha.On_RealUpdate += (room) =>
+        {
+            if (counter != 0) return;
+            for (int i = 0; i < room.updateList.Count; i++)
+            {
+                if (room.updateList[i] is Player p)
+                {
+                    var em = room.PlaySound(soundid, p.firstChunk, false, vol, pitch);
+                    counter = cooldown;
+                    return;
+                }
+            }
+        };
+        ha.On_CoreUpdate += (rwg) =>
+        {
+            if (counter > 0) counter--;
+        };
+    }
+
+    private static void Make_Rumble(Happen ha, string[] args)
+    {
+        int cooldown = 200,
+            duration = 80;
+        float intensity = 1f,
+            shake = 0.5f;
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            var spl = TXT.Regex.Split(args[i], "=");
+            if (spl.Length != 2) continue;
+            switch (spl[0].ToLower())
+            {
+                case "cooldown":
+                case "cd":
+                    int.TryParse(spl[1], out cooldown);
+                    break;
+                case "duration":
+                    int.TryParse(spl[1], out duration);
+                    break;
+                case "intensity":
+                    float.TryParse(spl[1], out intensity);
+                    break;
+                case "shake":
+                    float.TryParse(spl[1], out shake);
+                    break;
+            }
+        }
+
+        int counter = 0;
+        int active = duration;
+
+        //ha.On_CoreUpdate += (rwg) =>
+        //{
+        //    if (active > 0) active--;
+        //    if (counter > 0) counter--;
+        //    if (counter == 0)
+        //    {
+        //        active = duration;
+        //        counter = cooldown;
+        //    }
+
+        //};
+        ha.On_RealUpdate += (room) =>
+        {
+            //if (active != 0)
+            room.ScreenMovement(null, URand.insideUnitCircle * intensity, shake);
+        };
     }
 }
