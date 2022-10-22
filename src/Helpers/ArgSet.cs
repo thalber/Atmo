ecbd58fad3bed76864d2586029a63c11d0040de0
thalber <73020111+thalber@users.noreply.github.com>
2022-10-22 22:39:38 +0400

    docstrings

--- src/Helpers/ArgSet.cs
+++ src/Helpers/ArgSet.cs
@@ -7,9 +7,18 @@ using System.Text;
 namespace Atmo.Helpers;
 
 /// <summary>
-/// A set of <see cref="Arg"/>s. Implements IList, so can be indexed by position, as well as <see cref="Arg"/>'s name.
-/// ArgSets can be created from string arrays, explicitly (<see cref="ArgSet(string[])"/>) or implicitly (a cast that invokes the same constructor).
-/// When instantiating, ArgSet creates a dictionary-based index of named args. All args are created with linkage. 
+/// A set of <see cref="Arg"/>s. Implements IList, so can be indexed by position (<see cref="this[int]"/>), as well as <see cref="Arg"/>'s name <see cref="this[string[]]"/>.
+/// <para>
+///		ArgSets can be created from string arrays, explicitly (<see cref="ArgSet(string[], bool)"/>)
+///		or implicitly (a cast that invokes the same constructor). When instantiating, ArgSet creates 
+///		a dictionary-based index of named args. All args are created with linkage. 
+/// </para>
+/// <para>
+///		This class is typically used by delegates in <see cref="API"/>,
+///		although nothing stops a user from applying it in an unrelated context.
+///		Use of ArgSet over simple string arrays was enforced to ensure 
+///		consistent argument syntax in various <see cref="Happen"/> actions.
+/// </para>
 /// </summary>
 public sealed class ArgSet : IList<Arg>
 {
@@ -17,11 +26,12 @@ public sealed class ArgSet : IList<Arg>
 	/// Creates the instance from a given array of raw string arguments.
 	/// </summary>
 	/// <param name="rawargs"></param>
-	public ArgSet(string[] rawargs)
+	/// <param name="linkage">Whether to parse argument names and variable links on instantiation. On by default.</param>
+	public ArgSet(string[] rawargs, bool linkage = true)
 	{
 		for (int i = 0; i < rawargs.Length; i++)
 		{
-			Arg newarg = new(rawargs[i] ?? string.Empty, true);
+			Arg newarg = new(rawargs[i] ?? string.Empty, linkage);
 			_args.Add(newarg);
 			if (newarg.Name is not null) { _named.Add(newarg.Name, newarg); }
 		}
@@ -30,6 +40,15 @@ public sealed class ArgSet : IList<Arg>
 	private readonly Dictionary<string, Arg> _named = new();
 	/// <summary>
 	/// Checks given names and returns first matching named argument, if any.
+	/// <para>
+	/// This indexer can be used to easily fetch an argument that may have variant names, for example:
+	/// <code>
+	///		<see cref="ArgSet"/> set = myStringArray;
+	///		Arg
+	///		  cooldown = set["cd", "cooldown"] ?? 80,
+	///		  power = set["pow", "power"] ?? 0.3f;
+	/// </code>
+	/// </para>
 	/// </summary>
 	/// <param name="names">Names to check</param>
 	/// <returns>An <see cref="Arg"/> if one is found, null otherwise.</returns>
@@ -41,10 +60,13 @@ public sealed class ArgSet : IList<Arg>
 			return null;
 		}
 	}
-
 #pragma warning disable CS1591
 	#region interface
-	#region ilist
+	/// <summary>
+	/// Simple order-based indexing. For non-throwing alternative, you can use <see cref="Utils.AtOr{T}(IList{T}, int, T)"/>.
+	/// </summary>
+	/// <param name="index"></param>
+	/// <returns></returns>
 	public Arg this[int index] { get => _args[index]; set => _args[index] = value; }
 	public int Count 
 		=> _args.Count;
@@ -71,7 +93,6 @@ public sealed class ArgSet : IList<Arg>
 		=> _args.RemoveAt(index);
 	IEnumerator IEnumerable.GetEnumerator() 
 		=> _args.GetEnumerator();
-	#endregion ilist
 	#endregion interface;
 #pragma warning restore
 	/// <summary>
