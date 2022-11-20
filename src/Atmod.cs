30d5b000a39f8abe3a78ae19b5bd20c379ecd3f5
thalber <73020111+thalber@users.noreply.github.com>
2022-11-20 23:31:45 +0400

    Add AUDB data

--- src/Atmod.cs
+++ src/Atmod.cs
@@ -29,6 +29,26 @@ public sealed partial class Atmod : BaseUnityPlugin
 	/// Mod display name
 	/// </summary>
 	public const string DName = "Atmo";
+	#region AU
+	/// <summary>
+	/// AUDB update URL
+	/// </summary>
+	public string updateURL = "http://beestuff.pythonanywhere.com/audb/api/mods/5/4";
+	/// <summary>
+	/// AUDB Version
+	/// </summary>
+	public int version = 0;
+	/// <summary>
+	/// Public key segment
+	/// </summary>
+	public string keyE = "AQAB";
+	/// <summary>
+	/// Public key segment
+	/// </summary>
+	public string keyN = "uwptqosDNjimqNbRwCtJIKBXFsvYZN+b7yl668ggY46j+2Zlm/+L9TpypF6Bhu85CKnkY7ffFCQixTSzumdXrz1WVD0PTvoKDAp33U/loKHoAe/rs3HwdaOAdpug//rIGDmtwx56DC05NiLYKVRf4pS3yM1xN39Rr2at/RmAxdamKLUnoJtHRwx2eGsoKq5dmPZ7BKTmF/49N6eFUvUXEF9evPRfAdPH9bYAMNx0QS3G6SYC0IQj5zWm4FnY1C57lmvZxQgqEZDCVgadphJAjsdVAk+ZruD0O8X/dqXiIBSdEjZsvs4VDsjEF8ekHoon2UZnMEd6XocIK4CBqJ9HCMGaGZusnwhtVsGyMur1Go4w0CXDH3L5mKhcEm/V7Ik2RV5/Z2Kz8555fO7/9UiDC9vh5kgk2Mc04iJa9rcWSMfrwzrnvzHZzKnMxpmc4XoSqiExVEVJszNMKqgPiQGprkfqCgyK4+vbeBSXx3Ftalncv9acU95qxrnbrTqnyPWAYw3BKxtsY4fYrXjsR98VclsZUFuB/COPTI/afbecDHy2SmxI05ZlKIIFE/+yKJrY0T/5cT/d8JEzHvTNLOtPvC5Ls1nFsBqWwKcLHQa9xSYSrWk8aetdkWrVy6LQOq5dTSD4/53Tu0ZFIvlmPpBXrgX8KJN5LqNMmml5ab/W7wE=";
+	// ------------------------------------------------
+	#endregion AU
+
 	/// <summary>
 	/// Static singleton
 	/// </summary>
